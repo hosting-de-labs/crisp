e1ae0591258b105fb629710779c8ad3da2f9c7f5
Hendrik Sokolowski <h.sokolowski@hosting.de>
2021-07-09 14:52:41 +0200

    Use hooks instead of interceptor

--- api/crisp/client.go
+++ api/crisp/client.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"path"
 
 	"github.com/twitchtv/twirp"
 )
 
 const (
+	// DefaultAuthHeader defaults the default authentication header used to authorize requests
 	DefaultAuthHeader string = "Authorization"
 )
 
@@ -20,24 +22,35 @@ func NewCrispClient(baseURL string, token string) *Client {
 	c := NewCrispProtobufClient(
 		baseURL,
 		&http.Client{},
-		twirp.WithClientInterceptors(authInterceptor(DefaultAuthHeader, token)),
+		twirp.WithClientHooks(hooks(token)),
 	)
 
 	return &Client{c}
 }
 
-func authInterceptor(headerName string, token string) twirp.Interceptor {
-	return func(next twirp.Method) twirp.Method {
-		return func(ctx context.Context, request interface{}) (interface{}, error) {
-			header := make(http.Header)
-			header.Set(headerName, token)
-
-			ctx, err := twirp.WithHTTPRequestHeaders(ctx, header)
-			if err != nil {
-				return nil, fmt.Errorf("twirp error setting headers: %s", err)
+func hooks(token string) *twirp.ClientHooks {
+	return &twirp.ClientHooks{
+		RequestPrepared: func(ctx context.Context, request *http.Request) (context.Context, error) {
+			rqMethod := path.Base(request.RequestURI)
+			if rqMethod != "Register" {
+				return withAuthContext(ctx, DefaultAuthHeader, token)
 			}
 
-			return next(ctx, request)
-		}
+			return ctx, nil
+		},
+		ResponseReceived: nil,
+		Error:            nil,
 	}
+}
+
+func withAuthContext(ctx context.Context, headerName string, token string) (context.Context, error) {
+	header := make(http.Header)
+	header.Set(headerName, token)
+
+	ctx, err := twirp.WithHTTPRequestHeaders(ctx, header)
+	if err != nil {
+		return nil, fmt.Errorf("twirp error setting headers: %s", err)
+	}
+
+	return ctx, nil
 }
